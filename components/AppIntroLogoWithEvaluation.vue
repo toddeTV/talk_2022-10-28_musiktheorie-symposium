aa27f6084b5b1fcca7551ff8f81372f1be0b6f76
Thorsten Seyschab <business@todde.tv>
2022-10-27 20:32:25 +0200

    Added custom components.

---
+++ components/AppIntroLogoWithEvaluation.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="w-full h-full flex flex-col justify-center">
+    <div class="w-full flex flex-row justify-center">
+      <img src="/assets/fmadb_logo/v8/logo-light/logo-light_icon-text_h-256px.png" class="w-1/2" />
+    </div>
+    <div class="w-full flex flex-row justify-center pl-1/2" style="margin-left: -80px; font-size: 19px;">
+      <AppTextTypewriter text="The Film Music Analysis Database|Die Film Musik Analyse Datenbank" />
+    </div>
+
+    <div class="w-full flex flex-row justify-center mt-15">
+      <div class="flex flex-row text-xl">
+        <div>
+          <mdi-arrow-right-thick />
+        </div>
+        <div class="ml-2 mr-4">
+          Evaluation unter:
+          <br>
+          Deadline:
+        </div>
+        <div>
+          <a href="https://evaluation.fmadb.org/">https://evaluation.fmadb.org/</a>
+          <br>
+          2022-11-06
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
